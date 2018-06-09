b0d45ab1c96cf7eda5d434dced75e578d8875ab0
Peter Schmidbauer <peter.schmidb@gmail.com>
2018-06-09 19:43:37 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='achtungkurve',
+      version='0.1',
+      description='Multiplayer Tron AI Agent playground',
+      url='https://github.com/2er0/achtung-kurve',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=["numpy"],
+      zip_safe=False
+      )
