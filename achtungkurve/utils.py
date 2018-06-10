7f10d455601ebaa16098511357022942320fa31d
David Baumgartner <2er0@dbaumi.at>
2018-06-10 19:10:48 +0200

    quick utils for typings

---
+++ achtungkurve/utils.py
@@ -0,0 +1,13 @@
+# quick State class
+class State:
+    position: (1, 2)
+    alive: True
+    board: [[]]
+    last_alive: True
+
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
+# agent actions
+ACTIONS = ["left", "forward", "right"]
