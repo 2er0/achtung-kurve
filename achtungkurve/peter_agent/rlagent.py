462143037befe21a728e5e5020f9bde33608a5d8
Peter Schmidbauer <peter.schmidb@gmail.com>
2018-06-26 23:02:37 +0200

    add new reward calculation for rlagent

--- achtungkurve/peter_agent/rlagent.py
+++ achtungkurve/peter_agent/rlagent.py
@@ -87,9 +87,12 @@ class TronEnv(Env):
     def __init__(self, agent: QueueAgent):
         self.action_space = DiscreteSpace(3)
         self.agent = agent
+        self.alive_opponents = None
+        self.playing_opponents = None
         self.state = None
 
     def render(self, mode='human', close=False):
+        # board = self._board_from_state()
         board = np.array(self.state["board"])
         print(str(np.rot90(board)).replace('0', '-'))
         print()
@@ -109,12 +112,13 @@ class TronEnv(Env):
 
         board = self._board_from_state()
 
+        score = self._calc_reward()
+
         alive = self.state["alive"]
         game_over = self.state["game_over"]
-        last_alive = self.state["last_alive"]
-        score = 1 if last_alive else 0.1 if alive else -1.
-
         done = game_over or not alive
+
+        self.alive_opponents = self._count_opponents()
 
         return board, score, done, {}
 
@@ -125,7 +129,36 @@ class TronEnv(Env):
         while not self.state["alive"]:
             self.state = self.agent.take_action(None)
 
-        return np.array(self._board_from_state())
+        self.alive_opponents = self._count_opponents()
+        self.playing_opponents = self.alive_opponents
+
+        return self._board_from_state()
+
+    def _calc_reward(self):
+        if self.state["last_alive"] and self.playing_opponents > 0:
+            return 1
+
+        score = 0
+
+        if not self.state["alive"]:
+            score += -1
+        else:
+            # small bonus for staying alive, magnitude depending on board size
+            # capturing 1/num_players of the board yields 1 reward
+            inner_board_len = np.array(self.state["board"]).shape[0] - 2
+            total_board_air = (inner_board_len ** 2) - 4
+            score += (self.playing_opponents + 1) / total_board_air
+
+        current_alive = self._count_opponents()
+
+        # extra reward when opponents die
+        score += (self.alive_opponents - current_alive) * 0.25
+
+        return score
+
+    def _count_opponents(self) -> int:
+        board = np.array(self.state["board"])
+        return ((board == 5) | (board == 6) | (board == 7) | (board == 8)).sum()
 
     def _board_from_state(self):
         return np.array(self.state["board"])
@@ -171,38 +204,44 @@ def run_agent(agent):
     config.gpu_options.allow_growth = True
     set_session(tf.Session(config=config))
 
-    WINDOW_LENGTH = 4
+    WINDOW_LENGTH = 1
 
     num_actions = 3
-    board_shape = (5, 5)
-    input_shape = (WINDOW_LENGTH,) + board_shape
+    view_shape = (11, 11)
+    input_shape = (WINDOW_LENGTH,) + view_shape
 
-    env = RestrictedViewTronEnv(agent, 2)
+    env = RestrictedViewTronEnv(agent, 5)
 
     model = Sequential()
 
     model.add(Permute((2, 3, 1), input_shape=input_shape))
 
-    # model.add(Conv2D(64, (3, 3), padding="same"))
-    # model.add(Activation("relu"))
+    model.add(Conv2D(32, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+
+    model.add(Conv2D(64, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+
+    model.add(Conv2D(128, (3, 3), padding="same"))
+    model.add(Activation("relu"))
     model.add(Flatten())
 
-    model.add(Dense(32))
+    model.add(Dense(512))
     model.add(Activation("relu"))
 
     model.add(Dense(num_actions, activation="linear"))
 
     np.random.seed(2363)
 
-    policy = LinearAnnealedPolicy(BoltzmannQPolicy(), attr='tau', value_max=10.,
-                                  value_min=.1, value_test=1, nb_steps=1000000 // 10)
+    policy = LinearAnnealedPolicy(BoltzmannQPolicy(), attr='tau', value_max=2.,
+                                  value_min=.1, value_test=.1, nb_steps=1000000 // 10)
 
     processor = TronProcessor()
 
-    memory = SequentialMemory(limit=1000000 // 10, window_length=WINDOW_LENGTH)
+    memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
 
     dqn = DQNAgent(model, nb_actions=num_actions, policy=policy, memory=memory, processor=processor,
-                   nb_steps_warmup=50000, gamma=.9, target_model_update=10000,
+                   nb_steps_warmup=50000 // 5, gamma=.9, target_model_update=10000,
                    train_interval=4, delta_clip=1.)
 
     dqn.compile(Adam(lr=.00025), metrics=["mae"])
@@ -227,8 +266,13 @@ def run_agent(agent):
         dqn.load_weights('tmp/dqn_test_weights.h5f')
         dqn.test(env, nb_episodes=200000, visualize=False)
 
-    opponent()
-    train(True)
+    def test():
+        dqn.load_weights('tmp/dqn_test_weights.h5f')
+        dqn.test(env, nb_episodes=20, visualize=True)
+
+    # opponent()
+    # train(True)
+    test()
 
 
 if __name__ == "__main__":
