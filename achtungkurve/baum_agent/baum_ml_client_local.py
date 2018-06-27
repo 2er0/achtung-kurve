9f5ffaa7d1176ee9934faaef79f5aba088f77909
David Baumgartner <2er0@dbaumi.at>
2018-06-27 11:40:41 +0200

    some changes

--- achtungkurve/baum_agent/baum_ml_client_local.py
+++ achtungkurve/baum_agent/baum_ml_client_local.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     with BaumMlAgent() as agent:
 
         coro = loop.create_connection(lambda: AgentProtocol(agent, loop),
-                                      'home.dbaumi.at', '45555')
-                                      #'localhost', SERVER_PORT)
+                                      #'home.dbaumi.at', '45555')
+                                      'localhost', SERVER_PORT)
         loop.run_until_complete(coro)
         loop.run_forever()
         loop.close()
