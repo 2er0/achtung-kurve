608bde2d2f5df437abe983e323062fda63a5fd0a
Manuel Knoll <s1710595008@students.fh-hagenberg.at>
2018-07-22 17:04:03 +0200

    Add optional inputs for knoll_agent

--- achtungkurve/knoll_agent/restore.py
+++ achtungkurve/knoll_agent/restore.py
@@ -4,33 +4,32 @@ from achtungkurve.gameengine import Direction, BoardSquare
 from achtungkurve.server import SERVER_PORT
 from functools import partial
 from deap import algorithms, base, creator, tools, gp
-import operator
 import numpy as np
 import asyncio
-import random
 import dill as pickle
-import json
-import marshal, types
+import traceback
 
 
-with open('best_individual.pkl', 'rb') as in_:
+with open('best_individual_False_False_0.pkl', 'rb') as in_:
     best = pickle.load(in_)
-print(best)
-#    with open('best_individual.json', 'fb') as in_:
-#        best = json.loads(s)
-#    print(best)
-
-input()
-
+    
 loop = asyncio.new_event_loop()
 
-coro = loop.create_connection(lambda: TronGame(best, loop),
-                          'localhost', SERVER_PORT)
-
+while True:
+    try:
+        coro = loop.create_connection(lambda: TronGame(best, loop, True),
+                                        'astra.dbaumi.at', '44480')
+ #                                       'localhost', '15554')
+        loop.run_until_complete(coro)
+        break
+    except KeyboardInterrupt:
+        raise
+    except:
+        traceback.print_exc()
+print("connected")
 try:
     while(True):
         best.tiles_claimed = 0
-        loop.run_until_complete(coro)
         loop.run_forever()
         print(best.tiles_claimed)
 finally:
